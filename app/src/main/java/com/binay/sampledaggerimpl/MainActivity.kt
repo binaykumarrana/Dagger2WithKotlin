331a54a4213cd050302e5d21d23bc92c48a99271
Binay <binaykumarrana@gmail.com>
2020-06-23 23:28:15 +0800

    Add field injection in this commit from previous removing constructor injection

--- app/src/main/java/com/binay/sampledaggerimpl/MainActivity.kt
+++ app/src/main/java/com/binay/sampledaggerimpl/MainActivity.kt
@@ -4,16 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.binay.sampledaggerimpl.di.DaggerCarComponent
 import com.binay.sampledaggerimpl.model.Car
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var car: Car
+    @Inject
+    lateinit var car: Car
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val component = DaggerCarComponent.create()
-        car = component.getCar()
+        component.inject(this)
         car.drive()
     }
 }
--- app/src/main/java/com/binay/sampledaggerimpl/di/CarComponent.kt
+++ app/src/main/java/com/binay/sampledaggerimpl/di/CarComponent.kt
@@ -1,5 +1,6 @@
 package com.binay.sampledaggerimpl.di
 
+import com.binay.sampledaggerimpl.MainActivity
 import com.binay.sampledaggerimpl.model.Car
 import dagger.Component
 
@@ -7,4 +8,5 @@ import dagger.Component
 @Component
 interface CarComponent {
     fun getCar(): Car
+    fun inject(mainActivity: MainActivity)
 }
